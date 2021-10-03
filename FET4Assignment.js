1ddc7e82e476e8836ab63648d36690efc84e06b6
Jonathan Walsh <87747751+Falidian@users.noreply.github.com>
2021-10-02 22:15:41 -0600

    Complete Project

---
+++ FET4Assignment.js
@@ -0,0 +1,33 @@
+var id = 0;
+
+document.getElementById("add").addEventListener("click", () => {
+    let createdDate = new Date();
+    let table = document.getElementById("list") 
+    let row = table.insertRow(1); 
+    row.setAttribute("id", `item-${id}`);
+    row.insertCell(0).innerHTML = document.getElementById("new-vfn").value; 
+    row.insertCell(1).innerHTML = document.getElementById("new-vln").value;
+    row.insertCell(2).innerHTML = document.getElementById("new-start-date").value;
+    row.insertCell(3).innerHTML = document.getElementById("new-end-date").value;
+    row.insertCell(4).innerHTML = document.getElementById("new-site").value;
+    row.insertCell(5).innerHTML = document.getElementById("new-duty").value;
+    let actions = row.insertCell(6);
+    actions.appendChild(createDeleteButton(id++)); 
+    document.getElementById("new-vfn").value = "";
+    document.getElementById("new-vln").value = "";
+    document.getElementById("new-site").value = "";
+    document.getElementById("new-duty").value = "";
+});
+
+function createDeleteButton(id) {
+    let btn = document.createElement("button");
+    btn.className = "btn btn-primary";
+    btn.id = id;
+    btn.innerHTML = "Delete";
+    btn.onclick = () => {
+        console.log(`Deleting row with id: item-${id}`);
+        let elementToDelete = document.getElementById(`item-${id}`);  
+        elementToDelete.parentNode.removeChild(elementToDelete);
+    };
+    return btn;
+}
